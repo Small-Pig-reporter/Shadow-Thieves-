1394237405f499357215c4c5809202c7c94a00aa
Azizkhasyi11 <74176356+Azizkhasyi11@users.noreply.github.com>
2021-01-10 16:50:41 +0700

    Update server.js

--- server.js
+++ server.js
@@ -13,3 +13,18 @@ bot.status({
       time: 12
 })
 
+const fs = require('fs')
+
+const folders = fs.readdirSync("./commands/")
+
+for (const files of folders) {
+const folder = fs.readdirSync(`./commands/${files}/`).filter(file => file.endsWith(".js"))
+
+for (const commands of folder) {
+const command = require(`./commands/${files}/${commands}`) 
+bot.command({
+name: command.name,
+code: command.code
+})
+} 
+}
