d7b2d5d544b21d483c3f10e07a176dce366c687c
Azizkhasyi11 <74176356+Azizkhasyi11@users.noreply.github.com>
2021-01-10 16:43:06 +0700

    Update official.js

--- commands/other/official.js
+++ commands/other/official.js
@@ -1,4 +1,4 @@
 module.exports = ({
 name: "Wiki Changes",
-code: Hi! This is the recent charges of Shadow Thieves Wiki. https://shadowthieves.fandom.com/wiki/Special:RecentChanges
+code: `Hi! This is the recent charges of Shadow Thieves Wiki. https://shadowthieves.fandom.com/wiki/Special:RecentChanges`
 })
