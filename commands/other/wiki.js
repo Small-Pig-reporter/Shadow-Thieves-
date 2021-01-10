51f1e91c8f347260a322558d8410f30ee8384b1f
Azizkhasyi11 <74176356+Azizkhasyi11@users.noreply.github.com>
2021-01-10 16:41:39 +0700

    Update and rename wiki to wiki.js

--- commands/other/wiki.js
+++ commands/other/wiki.js
@@ -1,4 +1,4 @@
-bot.command({
+module.exports = ({
      name: "Wiki",
      code: A website or database developed callaboratively by a community of users, allowing any user to add any edit content.
 })
