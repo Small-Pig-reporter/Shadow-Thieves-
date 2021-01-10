4e6c7d43117e7751e339a48d42074aa3ce6fe96f
Azizkhasyi11 <74176356+Azizkhasyi11@users.noreply.github.com>
2021-01-10 16:44:10 +0700

    Update wiki.js

--- commands/other/wiki.js
+++ commands/other/wiki.js
@@ -1,4 +1,4 @@
 module.exports = ({
      name: "Wiki",
-     code: A website or database developed callaboratively by a community of users, allowing any user to add any edit content.
+     code: `A website or database developed callaboratively by a community of users, allowing any user to add any edit content.`
 })
