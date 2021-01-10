fa3fdcf3469e354cf78cb928fc647d46f4417760
Azizkhasyi11 <74176356+Azizkhasyi11@users.noreply.github.com>
2021-01-10 16:43:47 +0700

    Update officialwiki.js

--- commands/other/officialwiki.js
+++ commands/other/officialwiki.js
@@ -1,4 +1,4 @@
-bot.command({
+module.exports = ({
      name: "Official wiki",
-     code: aFans and Gamers Hub official wiki is Shadow Thieves Wiki. U can check out at here: https://shadowthieves.fandom.com/wiki/Small_Pig_Announce_News_Wiki
+     code: `aFans and Gamers Hub official wiki is Shadow Thieves Wiki. U can check out at here: https://shadowthieves.fandom.com/wiki/Small_Pig_Announce_News_Wiki`
 })
