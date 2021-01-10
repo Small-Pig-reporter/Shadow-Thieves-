f5545da351e9287686739d1f6a4bb7f6f5f96880
Azizkhasyi11 <74176356+Azizkhasyi11@users.noreply.github.com>
2021-01-10 16:38:22 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const dbd = require("dbd.js")
  
 const bot = Shadow Thieves wiki bot({
-token: "Nzc1MjQ1NjY1MTg1MDM4MzU2.X6jhyQ.7RDPtqJssLbzEsapWlNJzpuY454", 
+token: process.env.TOKEN, 
 prefix: "Wiki" 
 })
  
