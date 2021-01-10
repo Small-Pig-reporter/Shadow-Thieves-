1e7796a0e2af3dfb39a71c4925e9303f7a5bb368
Azizkhasyi11 <74176356+Azizkhasyi11@users.noreply.github.com>
2021-01-10 16:47:22 +0700

    Update and rename Blocked user to commands/mod/block.js

---
+++ commands/mod/block.js
@@ -0,0 +1,5 @@
+module.exports = ({
+name: "Blocked User",
+code: `There are some user got blocked for some reason at Shadow Thieves Wiki. Mostly from a community called PJ Masks Wiki. 
+https://shadowthieves.fandom.com/wiki/Special:Blocked_Users`
+})
